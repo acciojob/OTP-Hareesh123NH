6e03ee9e0e10ac5e8500fe0d1318418adb5c0ecb
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-10-09 14:25:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 const inputs=document.querySelectorAll(".code");
 inputs.forEach((input,i)=>{
-    input.addEventListener("input",()=>{
-        if(input.value.length===1 && i<inputs.length){
-            inputs[i+1].focus();
+    input.addEventListener("keydown",(e)=>{
+        if(e.key<=9 && e.key>=0 && i<inputs.length){
+			setTimeout(()=>{
+				 inputs[i+1].focus();
+			},100);
         }
     })
     input.addEventListener("keydown",(e)=>{
